73a99acd4e41461b633ebbb2e9b5c00996c0d5d1
Paolo Matthew Tolentino <tolentinopaolomatthew23@gmail.com>
2021-03-16 05:57:02 +0800

    added youtube search route

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
+const YTSearchAPI = require("youtube-search-api");
 const Koa = require("koa");
-const Router = require('@koa/router');
-const logger = require("koa-logger")
-const bodyParser = require('koa-bodyparser')
-const ytdl = require("ytdl-core-discord")
+const Router = require("@koa/router");
+const logger = require("koa-logger");
+const bodyParser = require("koa-bodyparser");
+const ytdl = require("ytdl-core-discord");
 const app = new Koa();
 const router = new Router();
 
 router.post("/youtube", async (ctx, _) => {
-  const body = ctx.request.body
+  const body = ctx.request.body;
   // get the url
-  const url = body.videoURL
+  const url = body.videoURL;
   const videoInfo = await ytdl.getInfo(url);
 
-  ctx.body = videoInfo
+  ctx.body = videoInfo;
 });
 
-app.use(logger())
-app.use(bodyParser())
-app.use(router.routes())
-app.use(router.allowedMethods())
+router.get("/youtube-search", async (ctx, _) => {
+  const { searchTerm } = ctx.request.body;
+  const { items: searchResults } = await YTSearchAPI.GetListByKeyword(
+    searchTerm
+  );
 
-app.listen(4040)
+  // Only fetch video results
+  // we dont need channels
+  const filteredResults = searchResults.filter(
+    (result) => result.type === "video"
+  );
+
+  ctx.body = filteredResults ? filteredResults : [];
+});
+
+app.use(logger());
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Server running at localhost:4040");
+app.listen(4040);
